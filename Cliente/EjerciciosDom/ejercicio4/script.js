89bdda1e4809063cd9a777d122b567631bce9e04
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-10-21 19:20:00 +0200

    21102023

--- Cliente/EjerciciosDom/ejercicio4/script.js
+++ Cliente/EjerciciosDom/ejercicio4/script.js
@@ -1,34 +1,50 @@
 let imgs = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg"
+    "foto1.jpg",
+    "foto2.jpg",
+    "foto3.jpg",
+    "foto4.jpg",
+    "foto5.jpg",
+    "foto6.jpg",
+    "foto7.jpg",
+    "foto8.jpg"
 ];
+let index = 0;
+
+function actualizarBotones() {
+    // document.querySelectorAll("button")[0].disabled = index == imgs.length - 1;
+    // document.querySelectorAll("button")[1].disabled = index == 0;
+    let buttons = document.querySelectorAll("button");
+    buttons[0].setAttribute("disabled", index == imgs.length - 1);
+    buttons[1].setAttribute("disabled", index == 0);
+
+    switch (index) {
+        case 0:
+            buttons[0].removeAttribute("disabled");
+            buttons[1].setAttribute("disabled", true);
+        case imgs.length - 1:
+            buttons[0].setAttribute("disabled", true);
+            buttons[1].removeAttribute("disabled");
+        default:
+            buttons[0].removeAttribute("disabled");
+            buttons[1].removeAttribute("disabled");
+    }
+}
+
+function cambiarImagen(indice) {
+    let img = document.querySelector("img");
+    img.setAttribute("src", "fotos/" + imgs[indice]);
+    index = indice;
+    actualizarBotones();
+}
 
 function avanzar() {
-    let img = document.querySelector("img");
-    img.setAttribute("src", "fotos/" + imgs[parseInt((img.getAttribute("src").split(".")[0].split("/")[1] - 1) + 1)]);
-    if (img.getAttribute("src") == "fotos/8.jpg") {
-        document.querySelectorAll("button")[0].setAttribute("disabled", true);
-        document.querySelectorAll("button")[1].removeAttribute("disabled");
-    } else {
-        document.querySelectorAll("button")[0].removeAttribute("disabled");
-        document.querySelectorAll("button")[1].removeAttribute("disabled");
-    }
+    index = Math.min(index + 1, imgs.length - 1);
+    cambiarImagen(index);
 }
 
 function retroceder() {
-    let img = document.querySelector("img");
-    img.setAttribute("src", "fotos/" + imgs[(img.getAttribute("src").split(".")[0].split("/")[1] - 1) - 1]);
-    if (img.getAttribute("src") == "fotos/1.jpg") {
-        document.querySelectorAll("button")[1].setAttribute("disabled", true);
-        document.querySelectorAll("button")[0].removeAttribute("disabled");
-    } else {
-        document.querySelectorAll("button")[0].removeAttribute("disabled");
-        document.querySelectorAll("button")[1].removeAttribute("disabled");
-    }
+    index = Math.max(index - 1, 0);
+    cambiarImagen(index);
 }
+
+actualizarBotones();
