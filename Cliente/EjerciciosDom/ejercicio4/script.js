4b75596284e68cce27630c2cf276d9bc4ca6b688
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-10-22 17:18:07 +0200

    ej4

--- Cliente/EjerciciosDom/ejercicio4/script.js
+++ Cliente/EjerciciosDom/ejercicio4/script.js
@@ -1,53 +1,52 @@
 let imgs = [
-    "foto1.jpg",
-    "foto2.jpg",
-    "foto3.jpg",
-    "foto4.jpg",
-    "foto5.jpg",
-    "foto6.jpg",
-    "foto7.jpg",
-    "foto8.jpg"
+  "foto1.jpg",
+  "foto2.jpg",
+  "foto3.jpg",
+  "foto4.jpg",
+  "foto5.jpg",
+  "foto6.jpg",
+  "foto7.jpg",
+  "foto8.jpg",
 ];
 let index = 0;
 
 function actualizarBotones() {
-    // document.querySelectorAll("button")[0].disabled = index == imgs.length - 1;
-    // document.querySelectorAll("button")[1].disabled = index == 0;
-    let buttons = document.querySelectorAll("button");
-    buttons[0].setAttribute("disabled", index == imgs.length - 1);
-    buttons[1].setAttribute("disabled", index == 0);
+  // document.querySelectorAll("button")[0].disabled = index == imgs.length - 1;
+  // document.querySelectorAll("button")[1].disabled = index == 0;
+  let buttons = document.querySelectorAll("button");
+  buttons[0].setAttribute("disabled", index == imgs.length - 1);
+  buttons[1].setAttribute("disabled", index == 0);
 
-    switch (index) {
-        case 0:
-            buttons[0].removeAttribute("disabled");
-            buttons[1].setAttribute("disabled", true);
-            break;
-        case imgs.length - 1:
-            buttons[0].setAttribute("disabled", true);
-            buttons[1].removeAttribute("disabled");
-            break;
-        default:
-            buttons[0].removeAttribute("disabled");
-            buttons[1].removeAttribute("disabled");
-            break;
-    }
+  switch (index) {
+    case 0:
+      buttons[0].removeAttribute("disabled");
+      buttons[1].setAttribute("disabled", true);
+      break;
+    case imgs.length - 1:
+      buttons[0].setAttribute("disabled", true);
+      buttons[1].removeAttribute("disabled");
+      break;
+    default:
+      buttons[0].removeAttribute("disabled");
+      buttons[1].removeAttribute("disabled");
+  }
 }
 
 function cambiarImagen(indice) {
-    let img = document.querySelector("img");
-    img.setAttribute("src", "fotos/" + imgs[indice]);
-    index = indice;
-    actualizarBotones();
+  let img = document.querySelector("img");
+  img.setAttribute("src", "fotos/" + imgs[indice]);
+  index = indice;
+  actualizarBotones();
 }
 
 function avanzar() {
-    index = Math.min(index + 1, imgs.length - 1);
-    cambiarImagen(index);
+  index = Math.min(index + 1, imgs.length - 1);
+  cambiarImagen(index);
 }
 
 function retroceder() {
-    index = Math.max(index - 1, 0);
-    cambiarImagen(index);
+  index = Math.max(index - 1, 0);
+  cambiarImagen(index);
 }
 
 actualizarBotones();
