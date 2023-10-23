e047ade9a4c66ee68e804f1ee47ed21dd80b0e2a
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-10-23 14:55:49 +0200

    peliculas

--- Servidor/Ejercicios/Formularios/usuario.php
+++ Servidor/Ejercicios/Formularios/usuario.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <? require "../Confidencial/base_de_datos.php" ?>
+    <?php require "../Confidencial/base_de_datos.php" ?>
 </head>
 
 <body>
