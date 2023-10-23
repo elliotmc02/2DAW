87b7244f6654080e39af9fd075c7cdc91d0cbd0b
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-10-23 19:59:34 +0200

    usuario form

--- Servidor/Ejercicios/Formularios/usuario.php
+++ Servidor/Ejercicios/Formularios/usuario.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php require "../Confidencial/base_de_datos.php" ?>
+    <?php require "../Confidencial/db_usuarios.php" ?>
 </head>
 
 <body>
