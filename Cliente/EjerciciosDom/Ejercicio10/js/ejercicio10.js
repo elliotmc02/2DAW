60daab850dafd082bf5b74193adb23d70d0ff441
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-10-31 10:51:38 +0100

    Ejercicio 10 con DOMContentLoaded y addeventListeners

--- Cliente/EjerciciosDom/Ejercicio10/js/ejercicio10.js
+++ Cliente/EjerciciosDom/Ejercicio10/js/ejercicio10.js
@@ -1,5 +1,12 @@
-const colorBody = window.getComputedStyle(document.body).getPropertyValue("background-color")
-function cambiarColor(div, raton) {
+document.addEventListener("DOMContentLoaded", () => {
+    const colorBody = window.getComputedStyle(document.body).getPropertyValue("background-color");
+    document.querySelectorAll("div > div").forEach((elemento) => {
+        elemento.addEventListener("mouseenter", () => cambiarColor(elemento.getAttribute("id"), "entrar", colorBody));
+        elemento.addEventListener("mouseleave", () => cambiarColor(elemento.getAttribute("id"), "salir", colorBody));
+    });
+});
+
+function cambiarColor(div, raton, colorBody) {
     const colorDiv = window.getComputedStyle(document.getElementById(div)).getPropertyValue("background-color");
     document.body.style.setProperty("background-color", (raton == "entrar" ? colorDiv : colorBody));
 }
