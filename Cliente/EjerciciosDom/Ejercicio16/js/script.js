8cdd4f27d0a85598119c566b3e31cf5317a4a207
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-11-07 11:30:50 +0100

    E16

--- Cliente/EjerciciosDom/Ejercicio16/js/script.js
+++ Cliente/EjerciciosDom/Ejercicio16/js/script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+  document.querySelectorAll("input").forEach((element) => {
+    element.addEventListener("click", () => mover(element));
   });
+});
+
+function mover(element) {
+  element.setAttribute("disabled", true);
+  let barra = element.previousElementSibling;
+  barra.setAttribute("value", 0);
+  progresar();
+  function progresar() {
+    barra.setAttribute("value", parseFloat(barra.getAttribute("value")) + Math.random() * 1);
+    if (parseInt(barra.getAttribute("value")) < 100) {
+      // setTimeout(() => {
+      requestAnimationFrame(progresar);
+      // }, 500);
+    } else {
+      element.removeAttribute("disabled");
+    }
+  }
+}
