cac01d71103e14d9d03459ae5d7a433e1aee6ba6
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-11-08 14:48:48 +0100

    a

--- Servidor/Ejercicios/Formularios/listado_peliculas2.php
+++ Servidor/Ejercicios/Formularios/listado_peliculas2.php
@@ -7,7 +7,7 @@
     <title>Listado peliculas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <?php require "../Confidencial/db_peliculas.php" ?>
-    <?php require "../Objetos/pelicula.php" ?>
+    <?php require "../Objetos/producto.php" ?>
 </head>
 
 <body>
