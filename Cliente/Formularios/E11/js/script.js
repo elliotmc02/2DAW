16195c011665a7eeb03971b02fda8cd27c89b90b
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-01-02 03:06:31 +0100

    E11

--- Cliente/Formularios/E11/js/script.js
+++ Cliente/Formularios/E11/js/script.js
@@ -1,37 +1,54 @@
-/**
- * 11.- Crea un formulario con los siguientes campos:
-• DNI: 8 números y una letra.
-• Nombre: uno o dos nombres.
-• Apellidos: uno o dos apellidos.
-• Fecha de nacimiento: formato dd/mm/yyyy.
-• Web personal: formato https://www.algo.tresletras
-• Contraseña: entre 8 y 12 caracteres.
-• Un botón para resetear el formulario.
-• Un botón para enviar el formulario.
-Haciendo uso de JavaScript pero sin usar expresiones regulares1, cuando se pulse el
-botón correspondiente, valida el formulario siguiendo las normas que se detallan a
-continuación y envía el resultado a otra página que muestre un mensaje de que todo ha
-salido bien.
-El formulario estará correcto sí y solo sí:
-• Ningún campo se queda vacío
-• El DNI tiene una longitud de 9
-• La fecha de nacimiento tiene el formato correcto.
-• Hay una o dos palabras en los campos nombre y apellidos.
-• El campo de la web debe empezar por https://
-• La longitud de la contraseña es correcta.
-Si algún campo se rellena de forma errónea, debe indicarse ese error usando un alert y
-devolviendo el foco al elemento erróneo (o al primer elemento erróneo).
-Como se ha comentado antes, debes preparar una sencilla pagina html con un mensaje
-de que todo ha ido bien y enlazarla al formulario de tal modo que aparezca cuando todos
-los campos sean correctos
- */
-
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('input[type="submit"]').addEventListener('click', comprobarCampos);
-    document.querySelector('input[name="reset"]').addEventListener('click', ev => ev.target.closest('form').reset());
+    // document.querySelector('input[name="resetButton"]').addEventListener('click', ev => ev.target.closest('form').reset());
 });
 
 const comprobarCampos = (ev) => {
     ev.preventDefault();
+    const form = ev.target.closest('form');
+    const campos = ['dni', 'nombre', 'apellido', 'fecha', 'web', 'contrasena'];
+    for (const elem of campos) {
+        const campo = form.elements[elem];
+        switch (elem) {
+            case 'dni':
+                if ([...campo.value.substring(0, 8)].some(elem => isNaN(elem)) || !isNaN(campo.value[8]) || campo.value.length != 9) {
+                    alerta(campo, 'El DNI debe tener 9 caracteres, y contener 1 letra al final');
+                    return;
+                }
+                break;
+            case 'nombre':
+            case 'apellido':
+                if (campo.value.split(' ').length > 2 || campo.value.length == 0) {
+                    alerta(campo, `El ${campo} debe tener 1 o 2 palabras`)
+                    return;
+                }
+                break;
+            case 'fecha':
+                const fechaSplit = campo.value.split('/');
+                if (fechaSplit.length != 3 || fechaSplit[2].length != 4 || fechaSplit.some(elem => isNaN(elem))) {
+                    alerta(campo, 'La fecha debe tener formato dd/mm/yyyy');
+                    return;
+                }
+                break;
+            case 'web':
+                if (!campo.value.startsWith('https://') || campo.value.substring(campo.value.lastIndexOf('.') + 1, campo.value.length).length != 3) {
+                    alerta(campo, 'La web debe empezar por https:// y acabar con tres letras');
+                    return;
+                }
+                break;
+            case 'contrasena':
+                if (campo.value.length < 8 || campo.value.length > 12) {
+                    alerta(campo, 'La contraseña debe tener entre 8 y 12 caraceteres');
+                    return;
+                }
+        }
+    }
 
+    window.location.href = 'correcto.html';
 };
+
+const alerta = (elem, msg) => {
+    alert(msg);
+    elem.focus();
+    elem.value = '';
+}
