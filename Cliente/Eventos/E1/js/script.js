e9a3057efe5d53d8e1e2a75da05a7f12c970d45f
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-01-11 00:37:42 +0100

    eventos

--- Cliente/Eventos/E1/js/script.js
+++ Cliente/Eventos/E1/js/script.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('section').addEventListener('click', (ev) => {
-        ev.target.style.setProperty('background-color', 'grey');
-    }, true);
+        ev.target.style.setProperty('background-color', 'gray');
+        console.log('section');
+        ev.stopPropagation();
+    }, false);
     document.querySelector('p').addEventListener('click', (ev) => {
         ev.target.style.setProperty('width', '50px');
-        ev.stopPropagation();
-    }, true);
+        console.log('parrafo');
+    }, false);
     document.querySelector('a').addEventListener('click', (ev) => {
         ev.preventDefault();
+        console.log('enlace');
         if (confirm('ir al enlace?')) window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
-    })
+    }, false);
 });
