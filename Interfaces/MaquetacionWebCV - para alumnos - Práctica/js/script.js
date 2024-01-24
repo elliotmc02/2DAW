d633682618ed5bbb19962d9e7c22e04fb99bd036
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-01-24 19:31:50 +0100

    popup, tabs y swiperjs

--- Interfaces/MaquetacionWebCV - para alumnos - Práctica/js/script.js
+++ Interfaces/MaquetacionWebCV - para alumnos - Práctica/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
+
+  $('#dialog').hide();
+  $('.certificates__certificate').click(() => {
+    $('#dialog').dialog();
+  });
+
   // fecha
   $('#fecha').datepicker();
 
@@ -18,4 +24,46 @@ $(document).ready(() => {
       $(':root').css('--color-text', '#555');
     }
   });
+
+  // Tabs
+  $('#tabs').tabs();
+
+  slideDown();
+  // Obtén el elemento
+  // var miElemento = $('.layout');
+
+  // // Define el radio y el ángulo inicial
+  // var radio = 100;
+  // var angulo = 0;
+  // var iteracionActual = 0;
+
+  // // Posición inicial fuera del área visible (izquierda)
+  // miElemento.css({
+  //   'left': '0',
+  // });
+
+  // function animarCirculo() {
+  //   angulo += 0.5;
+  //   var posX = 50 + radio * Math.cos(angulo);
+
+  //   var rotacion = angulo * (180 / Math.PI);
+
+  //   // Anima la propiedad 'left' para mover el elemento hacia el centro
+  //   miElemento.animate({
+  //     'left': posX + 'px',
+  //     'transform': 'rotate(' + rotacion + 'deg)'
+  //   }, 50, function () {
+  //     iteracionActual++;
+  //     animarCirculo();
+  //   });
+  // }
+
+  // animarCirculo();
+
 });
+
+const slideDown = () => {
+  $('.layout').css({
+    'top': '0',
+  });
+}
