266bc5348dff25744d5923e24d6bfa822283e3d7
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-02-13 01:16:33 +0100

    e4 sin hacer

--- Cliente/Consumo APIS/E4/script.js
+++ Cliente/Consumo APIS/E4/script.js
@@ -1,19 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarTareas();
+    fetchTareas();
+
+    document.querySelector('input[type="button"]').addEventListener('click', anadirTarea);
+
 });
 
-const mostrarTareas = () => {
-    // fetch('https://jsonplaceholder.typicode.com/todos')
-    fetch('test.json')
+const fetchTareas = async () => {
+    const tareas = await fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
-        .then(data => {
-            for (const tarea of data) {
-                console.log(tarea);
-                const li = document.createElement('li');
-                li.textContent = tarea;
-                document.querySelector('ul').appendChild(li);
-            }
-        })
-        .catch(error => console.log(`Error:${error}`))
+        .then(data => data)
+        .catch(error => console.log('error:', error))
+
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+    return tareas;
 }
 
+const obtenerDatos = () => {
+    const datosGuardados = localStorage.getItem('tareas')
+    console.log(datosGuardados);
+}
+
+const guardarDatos = tareas => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+}
+
+const insertarLista = tareas => {
+    const li = document.createElement('li');
+    li.textContent = tarea.title;
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    li.appendChild(checkbox);
+    document.querySelector('ul').appendChild(li);
+}
+
+const anadirTarea = () => {
+    const tarea = {
+        title: document.querySelector('input[type="text"]').value,
+        completed: false
+    }
+    insertarLista(tarea);
+    guardarDatos(tarea);
+}
